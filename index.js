31f93255dd2f7b2936a15078d9a9ceae20d6aca6
Guy Bianco IV <gjbiancoiv@gmail.com>
2020-04-29 11:47:10 -0400

    unwrap axios data

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const axios = require("axios");
 module.exports = (robot) => {
   robot.hear(/^!inspire$/i, (res) => {
     axios("https://inspirobot.me/api?generate=true")
+      .then((response) => response.data)
       .then(res.send)
       .catch((err) => {
         console.error(err);
