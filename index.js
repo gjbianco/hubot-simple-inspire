114ab73dffaa788d61f2efc6f2efae3e92dd5099
Guy Bianco IV <gjbiancoiv@gmail.com>
2020-04-29 14:36:24 -0400

    add evangelizing

--- index.js
+++ index.js
@@ -11,4 +11,20 @@ module.exports = (robot) => {
         res.send("error occurred trying to find inspiration D:");
       });
   });
+
+  robot.hear(/^!evangelize (\d)$/i, async (res) => {
+    let imgStr = "";
+    try {
+      for (let i = 0; i < res.match[1]; i++) {
+        const axiosResp = await axios(
+          "https://inspirobot.me/api?generate=true"
+        );
+        imgStr += axiosResp.data + "\n";
+      }
+      res.send(imgStr);
+    } catch (err) {
+      console.error(err);
+      res.send("error occurred evangelizing. donate more to the megachurch.");
+    }
+  });
 };
