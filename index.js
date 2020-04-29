2fb7db231b88b2b3ea69212ba371615c7de4cd4b
Guy Bianco IV <gjbiancoiv@gmail.com>
2020-04-29 11:44:40 -0400

    initial version

---
+++ index.js
@@ -0,0 +1,12 @@
+const axios = require("axios");
+
+module.exports = (robot) => {
+  robot.hear(/^!inspire$/i, (res) => {
+    axios("https://inspirobot.me/api?generate=true")
+      .then(res.send)
+      .catch((err) => {
+        console.error(err);
+        res.send("error occurred trying to find inspiration D:");
+      });
+  });
+};
