cf121fa380a37454a41178071da97976febcc95f
Guy Bianco IV <gjbiancoiv@gmail.com>
2020-04-29 11:48:43 -0400

    fix

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const axios = require("axios");
 module.exports = (robot) => {
   robot.hear(/^!inspire$/i, (res) => {
     axios("https://inspirobot.me/api?generate=true")
-      .then((response) => response.data)
-      .then(res.send)
+      .then((response) => {
+        res.send(response.data);
+      })
       .catch((err) => {
         console.error(err);
         res.send("error occurred trying to find inspiration D:");
